5b753122698984beef2c2726e8e1bc3a07dc931e
Eric Loots <eric.loots@gmail.com>
2014-11-28 17:24:51 +0100

    Alternative using sets - not particularly efficient, but done to demonstrate principle

--- src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
@@ -11,7 +11,9 @@ class Problem001 extends EulerSuite {
    * we get 3, 5, 6 and 9. The sum of these multiples is 23.
    * Find the sum of all the multiples of 3 or 5 below 1000.
    */
-  euler(problem(1)) {
-    TODO
+  euler(problem(1), "eloots") {
+    val multOf3 = (3 until 1000 by 3).toSet
+    val multOf5 = (5 until 1000 by 5).toSet
+    (multOf3 ++ multOf5).sum
   }
 }
