912d8b09767f7b1547bff68e2fff9a66166595fb
Sam De Backer <sam@ireact.be>
2014-11-28 12:22:49 +0100

    simple solution

--- src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
@@ -11,7 +11,9 @@ class Problem001 extends EulerSuite {
    * we get 3, 5, 6 and 9. The sum of these multiples is 23.
    * Find the sum of all the multiples of 3 or 5 below 1000.
    */
-  euler(problem(1)) {
-    TODO
+
+  euler(problem(1), "simple") {
+    // we first construct a Range, and then filter O(n), and sum O(n) + O(n * 8 / 15)
+    (1 until 1000).filter(l => l % 3 == 0 || l % 5 == 0).sum
   }
 }
