3e2af0d10685665b615402f20c9af2f54251d1c2
Olivier Samyn <olivier@26lights.com>
2014-11-28 17:48:18 +0100

    [#5] Using simple math properties.
    This does not teach you anything about scala but should probably be really fast.
    
    Some explanation: let's decompose the sum of the suite of multiples of 3 below 1000:
    ```3 + 6 + 9 + 12 + ... + 999```
    If we divide everything by 3, this can be rewritten as:
    ```3 * (1 + 2 + 3 + 4 + ... + 333)```
    The sum of such an integer suite is simple to compute:
    ```sum(1..n) = n * n+1 / 2```
    Now we can rewrite the suite of multiple of 3 below 1000 as:
    ```333 * (333 + 1) /2```
    Simple no ? Let's call this value 'sum3'
    
    Now, we can do the same with multiples of 5. Let's call that result 'sum5'.
    
    If you add sum3 and sum5, you will not find the expected value because multiples of 3 and 5 (ie multiples of 15) are taken two times into account.
    
    So, let's compute the sum of multiples of 15 below 1000 and call that variable sum15.
    
    Our final result will be: sum3 + sum5 - sum15.

--- src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
@@ -11,7 +11,17 @@ class Problem001 extends EulerSuite {
    * we get 3, 5, 6 and 9. The sum of these multiples is 23.
    * Find the sum of all the multiples of 3 or 5 below 1000.
    */
-  euler(problem(1)) {
-    TODO
+  euler(problem(1), "using math properties") {
+
+    // Count the number of multiples below 3, 5 and 15
+    val count3 = (1000-1) / 3
+    val count5 = (1000-1) / 5
+    val count15 = (1000-1) / 15
+
+    // Use the math property: sum(1..n) = (n * n+1) / 2
+    (( count3 * (count3 + 1) * 3 ) +
+     ( count5 * (count5 + 1) * 5 ) -
+     ( count15 * (count15 + 1) * 15 )) / 2
   }
+
 }
