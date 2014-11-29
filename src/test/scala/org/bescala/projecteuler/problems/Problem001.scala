4cc1a5a0896100f502564a00ba619f5d98ee35ae
LucDupAtGitHub <luc@strongtyped.io>
2014-11-29 22:56:49 +0100

    Problem001 and Problem003

--- src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem001.scala
@@ -3,6 +3,8 @@ package org.bescala.projecteuler.problems
 import org.bescala.projecteuler.EulerSuite
 import org.bescala.projecteuler.ProjectEuler._
 
+import scala.collection.immutable.Stream
+
 class Problem001 extends EulerSuite {
 
   /**
@@ -11,7 +13,27 @@ class Problem001 extends EulerSuite {
    * we get 3, 5, 6 and 9. The sum of these multiples is 23.
    * Find the sum of all the multiples of 3 or 5 below 1000.
    */
-  euler(problem(1)) {
-    TODO
+  val simpleSolution = s"simple solution for problem ${1}"
+
+  euler(problem(1), simpleSolution) {
+    (1 until 1000).filter { z =>
+      z % 3 == 0 || z % 5 == 0
+    }.foldLeft(0) { (z, y) =>
+      z + y
+    }
+  }
+
+  // thanks to @tdeconin
+  val streamBasedSolution = s"stream based solution for problem ${1}"
+
+  euler(problem(1), streamBasedSolution) {
+    val upperLimit = 1000
+    val divisors = Seq(3, 5)
+    Stream.from(1)
+      .takeWhile(_ < upperLimit)
+      .foldLeft(0) {
+      case (acc, i) if (divisors.exists(i % _ == 0)) => acc + i
+      case (acc, _) => acc
+    }
   }
 }
--- src/test/scala/org/bescala/projecteuler/problems/Problem003.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem003.scala
@@ -8,7 +8,17 @@ class Problem003 extends EulerSuite {
    * The prime factors of 13195 are 5, 7, 13 and 29.
    * What is the largest prime factor of the number 600851475143?
    */
-  euler(problem(3)) {
-    TODO
+  val someSolution = s"some solution for problem ${3}"
+
+  euler(problem(3), someSolution) {
+
+    import math._
+
+    def squareRootOf(z: Long) =
+      round(floor(sqrt(z.toDouble)))
+
+    val z = 600851475143L
+    (2L to squareRootOf(z)).filter(y => z % y == 0 && (2L to squareRootOf(y)).forall(y % _ != 0)).last
   }
+
 }
