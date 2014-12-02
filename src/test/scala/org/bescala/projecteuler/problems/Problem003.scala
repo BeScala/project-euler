48999550d288c042848f789790c93cda1165857e
LucDupAtGitHub <luc@strongtyped.io>
2014-12-02 21:59:26 +0100

    correction Problem003

--- src/test/scala/org/bescala/projecteuler/problems/Problem003.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem003.scala
@@ -4,24 +4,16 @@ import org.bescala.projecteuler.EulerSuite
 import org.bescala.projecteuler.ProjectEuler._
 
 import scala.annotation.tailrec
-import scala.annotation.tailrec
 
 class Problem003 extends EulerSuite {
   /**
    * The prime factors of 13195 are 5, 7, 13 and 29.
    * What is the largest prime factor of the number 600851475143?
    */
+
   val someSolution = s"some solution for problem ${3}"
 
   euler(problem(3), someSolution) {
-
-    import math._
-
-    def squareRootOf(z: Long) =
-      round(floor(sqrt(z.toDouble)))
-
-    def isPrime(z: Long): Boolean =
-      (2L to squareRootOf(z)).forall(z % _ != 0)
 
     def natsFrom(z: Long): Stream[Long] = {
       def loop(z: Long): Stream[Long] =
@@ -29,11 +21,21 @@ class Problem003 extends EulerSuite {
       loop(z)
     }
 
-    val primes =
-      natsFrom(2L).filter(isPrime)
+    def sieve(stream: Stream[Long], z: Long): Stream[Long] = {
+      @tailrec
+      def loop(stream: Stream[Long], y: Long)(acc: Stream[Long]): Stream[Long] = {
+        if (y == 1L) {
+          acc
+        }
+        else {
+          val p = stream.head
+          loop(stream.filter(_ % p != 0), if (y % p == 0) y / p else y)(p #:: acc)
+        }
+      }
+      loop(stream, z)(Stream())
+    }
 
-    primes.take(882).filter(600851475143L % _ == 0).toList.last
-
+    sieve(natsFrom(2), 600851475143L).head
   }
 
 }
