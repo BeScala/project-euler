0fa91229dbca2d76c337111d77a39c2b28e2981d
Eric Loots <eric.loots@gmail.com>
2014-12-03 10:26:58 +0100

    Added new Problem004: Largest palindrome product Added Scala language presentation Updated description of problem 4 in tests

---
+++ src/test/scala/org/bescala/projecteuler/problems/Problem004.scala
@@ -0,0 +1,30 @@
+package org.bescala.projecteuler.problems
+
+
+import org.bescala.projecteuler.EulerSuite
+import org.bescala.projecteuler.ProjectEuler._
+
+class Problem004 extends EulerSuite {
+
+ /**
+  * A palindromic number reads the same both ways. The largest palindrome made from the product
+  * of two 2-digit numbers is 9009 = 91 x 99.
+  *
+  * Task: Find the largest palindrome made from the product of two 3-digit numbers.
+  *
+  * Note: The stated problem can be solved for numbers with a larger number of digits.
+  *
+  * For example, for 5 digit numbers, the largest palindrome is 9966006699 = 99681 * 99979 and
+  * this solution can be determined programmatically in under a second.
+  *
+  * In fact, it is possible to calculate the value of these palindrome for the product of two
+  * numbers up-to 8 digits in around 5 seconds.
+  *
+  * Finding the right strategy for finding a fast solution is not straightforward. Use experiments to
+  *find a good strategy. The 'fast' solution doesn't require tons of math...
+  *
+  */
+  euler(problem(4)) {
+    TODO
+  }
+}
