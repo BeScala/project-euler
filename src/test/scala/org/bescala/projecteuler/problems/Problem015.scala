4a861a40d02c10c3b89b06c8af12c3ea047964cf
vrobinho <robaye.vincent@gmail.com>
2015-01-02 13:11:58 +0100

    #15: Using Pascal Triangle

--- src/test/scala/org/bescala/projecteuler/problems/Problem015.scala
+++ src/test/scala/org/bescala/projecteuler/problems/Problem015.scala
@@ -11,8 +11,12 @@ class Problem015  extends EulerSuite {
    *
    * How many such routes are there through a 20×20 grid?
    */
-  euler(problem(15)) {
+  euler(problem(15), "Naive recursive approach") {
     latticeCount(20,20)
+  }
+  
+  euler(problem(15), "Using Pascal Triangle") {
+    latticeCountUsingPascalTriangle(20)
   }
 
   /**
@@ -25,10 +29,29 @@ class Problem015  extends EulerSuite {
    */
   def latticeCount(r: Int, c: Int): Long = {
     def latticeAcc(r: Int, c: Int, acc: Long): Long = {
-      if (r == 0 && c == 0) 0
-      else if (r == 0 || c == 0) 1
+      if (r == 0 || c == 0) 1
       else latticeAcc(r, c-1, acc+1) + latticeAcc(r-1, c, acc+1)
     }
     latticeAcc(r, c, 0)
+  }
+
+  /**
+   *
+   * @param n an integer: the size of the pascal triangle
+   * @return the pascal triangle of [n] rows stored as a List(List(Long))
+   */
+  def pascalTriangle(n: Int): List[List[Long]] = {
+    def generateRow(n: Int, t: List[List[Long]]): List[List[Long]] = { 
+      n match {
+        case 1 => t
+        case n:Int => 
+          generateRow (n - 1, t :+ (List (1L) ::: t.last.zip (t.last.tail).map {case (a,b) => a+b} ::: List (1L) ) )
+      }
+    }
+    generateRow(n, List(List(1L)))
+  }
+
+  def latticeCountUsingPascalTriangle(size: Int) : Long = {
+    pascalTriangle(2 * size + 1).last.apply(size)
   }
 }
